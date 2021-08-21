8b82dfbd901dedc4d48f9134a010c5faff2eff4c
Marx Chryz <marxchryz30@gmail.com>
2021-08-21 18:00:32 +0800

    view users added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import { UserTable } from './UserTable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              View Users
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/create" className="nav-link">
+              Create User
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" component={UserTable} />
+      </Switch>
     </div>
   );
 }
---
+++ src/UserTable.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getUsers } from './api';
+
+export const UserTable = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      let users = [];
+      users = await getUsers();
+      setItems(users);
+    };
+    fetchItems();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="mt-3">
+        <h3>Users</h3>
+        <table className="table table-striped mt-3">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Middle Name</th>
+              <th>Last Name</th>
+              <th>Email Address</th>
+              <th>Mobile Number</th>
+              <th>Sex</th>
+              <th>Religion</th>
+              <th>Civil Status</th>
+              <th>Birthday</th>
+              <th>Birthplace</th>
+              <th>Height</th>
+              <th>Weight</th>
+              <th>Father</th>
+              <th>Mother</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((user) => (
+              <tr key={user._id}>
+                <td>{user.firstName}</td>
+                <td>{user.middleName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.mobile}</td>
+                <td>{user.sex}</td>
+                <td>{user.religion}</td>
+                <td>{user.civilStatus}</td>
+                <td>{user.birthday}</td>
+                <td>{user.birthplace}</td>
+                <td>{user.height}</td>
+                <td>{user.weight}</td>
+                <td>{user.father}</td>
+                <td>{user.mother}</td>
+                <td>
+                  <Link to={`/${user._id}`}>
+                    <button class="btn btn-primary">Edit</button>
+                  </Link>
+                  <button class="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
