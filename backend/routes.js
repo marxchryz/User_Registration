247087ac4ad2345dfdc3051ee0a1a450905b2a4f
Marx Chryz <marxchryz30@gmail.com>
2021-08-21 17:15:12 +0800

    api routes added

---
+++ backend/routes.js
@@ -0,0 +1,58 @@
+const { User } = require('./models');
+
+module.exports = (app) => {
+  app.get('/', async (req, res) => {
+    let users;
+    try {
+      users = await User.find();
+    } catch (err) {
+      throw err;
+    }
+    return res.json(users);
+  });
+
+  app.get('/:id', async (req, res) => {
+    let { id } = req.params;
+    let user;
+    try {
+      user = await User.findById(id);
+    } catch (err) {
+      throw err;
+    }
+    return res.json(user);
+  });
+
+  app.post('/', async (req, res) => {
+    let user = req.body;
+    try {
+      user = await User.create(user);
+    } catch (err) {
+      throw err;
+    }
+    return res.json(user);
+  });
+
+  app.delete('/:id', async (req, res) => {
+    let { id } = req.params;
+    let user;
+    try {
+      user = await User.findByIdAndDelete(id);
+    } catch (err) {
+      throw err;
+    }
+    if (user) return res.json({ deleted: true });
+    return res.json({ deleted: false });
+  });
+
+  app.post('/:id', async (req, res) => {
+    let user = req.body;
+    let { id } = req.params;
+    try {
+      user = await User.findByIdAndUpdate(id, user, { new: true });
+    } catch (err) {
+      throw err;
+    }
+    if (user) return res.json({ success: true, user });
+    return res.json({ success: false });
+  });
+};
